2599cf10a334e03b42b4aab1a1b3d654952e5a8c
Michal Dziurowski <mdziurowski@gmail.com>
2025-09-03 18:50:43 +0200

    changed node version to the one which match bitwarden

--- install/devel.sh
+++ install/devel.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/devel
 
 sudo pacman -S --noconfirm --needed go
-sudo pacman -S --noconfirm --needed nodejs
+sudo pacman -S --noconfirm --needed nodejs-lts-jod
 sudo pacman -S --noconfirm --needed npm
 
 curl -fsSL https://claude.ai/install.sh | bash
