1774234c4c70722d70edf9f04410198acccc8277
Michal Dziurowski <mdziurowski@gmail.com>
2025-09-04 18:02:45 +0200

    nicer sddm theme

--- install/new_user.sh
+++ install/new_user.sh
@@ -7,3 +7,13 @@ sudo systemctl disable omarchy-seamless-login
 # as a additional user:
 #symlink ~/.local/share/fonts
 #fc-cache
+
+yay -S sddm-silent-theme
+
+#and in /etc/sddm.conf (ofc without # ;) )
+#[General]
+#InputMethod=qtvirtualkeyboard
+#GreeterEnvironment=QML2_IMPORT_PATH=/usr/share/sddm/themes/silent/components/,QT_IM_MODULE=qtvirtualkeyboard
+#
+#[Theme]
+#Current=silent
