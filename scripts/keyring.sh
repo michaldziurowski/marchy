8595cb1fd00909835653c3aa82830a6f713862f1
Michal Dziurowski <mdziurowski@gmail.com>
2025-11-24 17:07:09 +0100

    fix command

--- scripts/keyring.sh
+++ scripts/keyring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S -noconfirm --needed seahorse #GUI for keys management
+sudo pacman -S --noconfirm --needed seahorse #GUI for keys management
 systemctl --user enable gcr-ssh-agent.socket
 systemctl --user start gcr-ssh-agent.socket
